6985f2ff3871007750971c81b456c6eb9d432e5d
Anthony Suzanne <anthonysuzanne@mbp-de-anthony.lan>
2019-11-12 11:57:05 +0100

    Components + Package : Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// --- Import des packages ---
+import React from "react";
+import axios from "axios";
+// --- Import du CSS ---
+import "./App.css";
+// --- Import des components ---
+import Header from "./components/Header";
+import Top from "./components/Top";
+import Section from "./components/Section";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Top />
+      <Section />
+      <Card />
+    </>
   );
 }
 
